0d236392f31d5e00919f87642e4fdf31d802c87e
Neverlanders <vanhoutte.tom@gmail.com>
2022-01-11 14:53:22 +0100

    login afgewerkt

--- admin/includes/Database.php
+++ admin/includes/Database.php
@@ -21,6 +21,10 @@
                 die("Query kan niet worden uitgevoerd " . $this->connection->error);
             }
         }
+        public function escape_string($string){
+            $escaped_string = $this->connection->real_escape_string($string);
+            return $escaped_string;
+        }
         function __construct(){
             $this->open_db_connection();
         }
--- admin/login2.php
+++ admin/login2.php
@@ -1,5 +1,25 @@
 <?php
     require_once("includes/header.php");
+    if($session->is_signed_in()){
+        redirect("index.php");
+    }
+    if(isset($_POST['submit'])){
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+        $user_found = User::verify_user($username,$password);
+        if($user_found)
+        {
+            $session->login($user_found);
+            redirect("index.php");
+        }else
+        {
+            $the_message = "your password or username FAILED!";
+        }
+    }
+    else{
+        $username="";
+        $password="";
+    }
 ?>
 <div class="container-fluid">
     <div class="row">
@@ -14,7 +34,7 @@
                     <input type="password" class="form-control" name="password">
                 </div>
                 <div class="form-group">
-                    <input type="submit" value="Submit" class="btn btn-primary">
+                    <input type="submit" value="Submit" name="submit" class="btn btn-primary">
                 </div>
             </form>
         </div>
