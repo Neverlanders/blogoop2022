17cfc87a5542a6f6900b8784f14418db5d6cc8eb
Neverlanders <vanhoutte.tom@gmail.com>
2022-01-11 10:58:21 +0100

    test

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -1,17 +1,45 @@
 <?php
     class User{
+    /*** PROPERTIES ****/
+         public $id;
+         public $username;
+         public $password;
+         public $first_name;
+         public $last_name;
+    /*** METHODS ***/
+        /**QUERY**/
+        public static function find_this_query($sql){
+            global $database;
+            $result = $database->query($sql);
+            $the_object_array = array();
+            while($row = mysqli_fetch_array($result)){
+                $the_object_array[] = self::instantie($row);
+            }
+            return $the_object_array;
+        }
         public static function find_all_users(){
-            global $database;
-            $result = $database->query("SELECT * FROM users");
-            return $result;
+            return self::find_this_query("SELECT * FROM USERS");
         }
-        /***
-        Functie find_user_by_id($user_id)
-         ***/
         public static function find_user_by_id($user_id){
-            global $database;
-            $result = $database->query("SELECT * FROM users WHERE id=$user_id");
-            return $result;
+           $result = self::find_this_query("SELECT * FROM users WHERE id=$user_id");
+           return !empty($result) ? array_shift($result) : false;
+
+           /* return self::find_this_query("SELECT * FROM users WHERE id=$user_id");*/
         }
+        /**CLASS**/
+        private function has_the_attribute($the_attribute){
+            $object_properties = get_object_vars($this);
+            return array_key_exists($the_attribute, $object_properties);
+        }
+        public static function instantie($result){
+            $the_object = new self();
+            foreach($result as $the_attribute => $value){
+                if($the_object->has_the_attribute($the_attribute)){
+                    $the_object->$the_attribute = $value;
+                }
+            }
+            return $the_object;
+        }
+
     }
 ?>
--- admin/includes/content.php
+++ admin/includes/content.php
@@ -3,57 +3,13 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <h1>testen connectie database</h1>
-            <hr>
-            <?php
-                if($database->connection){
-                    echo "ok, connectie gemaakt met de database";
-                }else{
-                    echo "geen databaseconnectie";
-                }
-            ?>
-            <hr>
+            <h1>User testen</h1>
+
             <h2>Ophalen van een user</h2>
-            <h3>Manier 1</h3>
-            <?php
-                $sql = "SELECT * FROM users WHERE id=1";
-                $result = $database->query($sql);
-                $user_found = mysqli_fetch_array($result);
-                echo $user_found["username"];
-            ?>
-            <hr>
-            <h3>Manier 2</h3>
             <?php
                 $result = User::find_user_by_id(1);
-                //var_dump($result);
-                while($row = mysqli_fetch_array($result)){
-                    echo $row["first_name"] . ' - ' . $row["last_name"] . "<br>";
-                }
+                echo $result->username;
             ?>
-            <hr>
-            <h2>Class user aanspreken en doorlopen</h2>
-            <h3>Manier 1</h3>
-            <ul class="list-group">
-           <?php
-               $user = new User();
-                $result = $user->find_all_users();
-                while($row = mysqli_fetch_array($result)){
-                    //echo $row["first_name"] . ' - ' . $row["last_name"] . "<br>";
-                    echo "<li class='list-group-item'>" . $row["first_name"] . ' - ' . $row["last_name"] ."</li>";
-                }
-            ?>
-            </ul>
-            <hr>
-            <h3>Manier 2</h3>
-            <ul class="list-group">
-            <?php
-            $result = User::find_all_users();
-            while($row = mysqli_fetch_array($result)){
-                //echo $row["first_name"] . ' - ' . $row["last_name"] . "<br>";
-                echo "<li class='list-group-item'>" . $row["first_name"] . ' - ' . $row["last_name"] ."</li>";
-            }
-            ?>
-            </ul>
 
         </div>
     </div>
