27325a7d05ce57e587aa2cf1410e686965d651ba
Neverlanders <vanhoutte.tom@gmail.com>
2022-01-19 10:52:26 +0100

    einde code begin upload

--- admin/includes/content.php
+++ admin/includes/content.php
@@ -3,13 +3,20 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
+            <?php echo IMAGES_PATH; ?>
            <?php
                 $photos = Photo::find_all();
                 foreach($photos as $photo){
                     echo $photo->title . "<br>";
                 }
            ?>
-
+           <?php
+                $photo = new Photo();
+                $photo->title = "Kastaar";
+                $photo->description="Lorem ipsum";
+                $photo->size = 15;
+                $photo->save();
+           ?>
 
         </div>
     </div>
